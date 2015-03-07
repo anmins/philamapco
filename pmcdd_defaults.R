f6f0316c4e0ff0ca998782901554d2457bdb0626
anmins <angelaminster@gmail.com>
2015-03-07 12:39:56 -0500

    I will finish this file one day

--- pmcdd_defaults.R
+++ pmcdd_defaults.R
@@ -1,5 +1,4 @@
-#This is a file of defaults for the phlamapco data dive blog
-
+#This is a file of defaults that I haven't finished writing
 
 #Primary Blue Red Color Scheme:
 col.br <- c("#0B94A5", "#89E4FF", "#2BDAF2", "#A53405", "#F2A183")
